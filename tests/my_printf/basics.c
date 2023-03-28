01c42b9745d666cac6696b3cd1ffbc0d16899ed3
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-28 04:24:47 +0200

    feat(main) additionnal tests for printf

--- tests/my_printf/basics.c
+++ tests/my_printf/basics.c
@@ -192,3 +192,72 @@ Test(my_printf_everything, basics, .init = redirect_all_std)
     my_printf("%d %s %f %b %o %x %X %u %e %E %g %G %p %c %ld %lld %lu %llu %% %", 42, "Hello World!", 3.141, 42, 42, 42, 42, 42, 42.42, 42.42, 42.42, 42.42, (int *)42, 'a', 42, 42, 42, 42, '%');
     cr_assert_stdout_eq_str("42 Hello World! 3.141000 101010 52 2a 2A 42 4.242000e+01 4.242000E+01 42.42 42.42 0x2a a 42 42 42 42 42 %");
 }
+
+
+Test(my_printf_test, basic_string_printing) {
+    cr_redirect_stdout();
+    my_printf("Hello, world!\n");
+    cr_assert_stdout_eq_str("Hello, world!\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, integer_printing) {
+    int x = 42;
+    cr_redirect_stdout();
+    my_printf("The value of x is: %d\n", x);
+    cr_assert_stdout_eq_str("The value of x is: 42\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, float_printing) {
+    double pi = 3.14159265359;
+    cr_redirect_stdout();
+    my_printf("The value of pi is: %f\n", pi);
+    cr_assert_stdout_eq_str("The value of pi is: 3.141593\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, character_printing) {
+    char c = 'a';
+    cr_redirect_stdout();
+    my_printf("The character is: %c\n", c);
+    cr_assert_stdout_eq_str("The character is: a\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, hexadecimal_printing) {
+    int x = 255;
+    cr_redirect_stdout();
+    my_printf("The value of x in hexadecimal is: %x\n", x);
+    cr_assert_stdout_eq_str("The value of x in hexadecimal is: ff\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, octal_printing) {
+    int x = 255;
+    cr_redirect_stdout();
+    my_printf("The value of x in octal is: %o\n", x);
+    cr_assert_stdout_eq_str("The value of x in octal is: 377\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, string_printing_with_width_and_precision) {
+    cr_redirect_stdout();
+    my_printf("The string is: %10.5s\n", "Hello, world!");
+    cr_assert_stdout_eq_str("The string is:      Hello\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, date_printing) {
+    int day = 28, month = 3, year = 2023;
+    cr_redirect_stdout();
+    my_printf("Today's date is: %02d/%02d/%04d\n", day, month, year);
+    cr_assert_stdout_eq_str("Today's date is: 28/03/2023\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, pointer_printing) {
+    int x = 42;
+    int *ptr = &x;
+    cr_redirect_stdout();
+    my_printf("The value of ptr is: %p\n", ptr);
+    cr_assert_stdout_eq_str("The value of ptr is: <address_of_ptr>\n", "Expected output does not match actual output");
+}
+
+Test(my_printf_test, negative_value_printing) {
+    cr_redirect_stdout();
+    my_printf("The value of x is: %d\n", -10);
+    cr_assert_stdout_eq_str("The value of x is: -10\n", "Expected output does not match actual output");
+}
