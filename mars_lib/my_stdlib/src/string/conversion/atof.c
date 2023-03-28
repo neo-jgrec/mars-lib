bb17b837796d9c9b47075bd956cc5ce43f0c6887
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-28 03:45:29 +0200

    fix(main) atof fix

--- mars_lib/my_stdlib/src/string/conversion/atof.c
+++ mars_lib/my_stdlib/src/string/conversion/atof.c
@@ -5,22 +5,45 @@
 ** atof
 */
 
+#include "macros.h"
+#include <stdint.h>
+
+static float my_powf(float base, int exponent)
+{
+    float result = 1;
+
+    if (exponent < 0) {
+        exponent = -exponent;
+        base = 1.0f / base;
+    }
+    while (exponent) {
+        if (exponent & 1)
+            result *= base;
+        exponent >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
 float my_atof(const char *str)
 {
-    int i = 0;
+    int32_t mantissa = 0;
+    int exponent = 0;
     int sign = 1;
-    float nb = 0;
-    float dec = 0;
+    float result;
 
-    for (; str[i] == '-' || str[i] == '+'; i++) {
-        if (str[i] == '-')
-            sign *= -1;
+    for (; *str == ' '; str++);
+    if (*str == '-' || *str == '+') {
+        sign = (*str == '-') ? -1 : 1;
+        str++;
     }
-    for (;str[i] >= '0' && str[i] <= '9'; i++)
-        nb = nb * 10 + str[i] - '0';
-    if (str[i] == '.')
-        i++;
-    for (int j = 1; str[i] >= '0' && str[i] <= '9'; j++, i++)
-        dec = dec * 10 + str[i] - '0';
-    return ((nb + (dec / 10)) * sign);
+    for (; IS_NUM(*str); str++)
+        mantissa = mantissa * 10 + (*str - '0');
+    if (*str == '.') {
+        str++;
+        for (; IS_NUM(*str); str++, exponent--)
+            mantissa = mantissa * 10 + (*str - '0');
+    }
+    result = (float)mantissa * my_powf(10, exponent) * sign;
+    return result;
 }
