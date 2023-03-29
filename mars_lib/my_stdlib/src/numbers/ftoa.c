ef8e69dbe5527261d3421c2d53358868fad3dd5a
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-03-29 15:15:10 +0200

    fix(main) ftoa

--- mars_lib/my_stdlib/src/numbers/ftoa.c
+++ mars_lib/my_stdlib/src/numbers/ftoa.c
@@ -7,25 +7,25 @@
 
 #include "my_stdlib.h"
 
-char *my_ftoa(float nb, int precision)
+char *my_ftoa(double nb, int precision)
 {
-    char *str = malloc(sizeof(char) * (precision + 2));
+    char *str = malloc(sizeof(char) * (precision * 10));
     int i = 0;
     int nb_int = (int)nb;
-    float nb_float = nb - nb_int;
+    double nb_double = nb - nb_int;
 
     if (str == NULL)
         return (NULL);
     (nb < 0) ? str[i++] = '-' : 0;
     nb_int = (nb_int < 0) ? -nb_int : nb_int;
-    nb_float = (nb_float < 0) ? -nb_float : nb_float;
+    nb_double = (nb_double < 0) ? -nb_double : nb_double;
     my_strcpy(str + i, my_itoa(nb_int));
     i += my_strlen(str + i);
     str[i++] = '.';
     for (int j = 0; j < precision; j++) {
-        nb_float *= 10;
-        str[i++] = (int)nb_float + '0';
-        nb_float -= (int)nb_float;
+        nb_double *= 10;
+        str[i++] = (int)nb_double + '0';
+        nb_double -= (int)nb_double;
     }
     str[i] = '\0';
     return (str);
